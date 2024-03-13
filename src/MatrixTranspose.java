74ece5dc868838807203dfb454009b06a87c9d40
Игорь <flagmr040@gmail.com>
2024-03-13 12:45:17 +0200

    bugs fixed

--- src/MatrixTranspose.java
+++ src/MatrixTranspose.java
@@ -1,8 +1,10 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MatrixTranspose {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         System.out.println("Введіть кількість рядків (M):");
         int m = scanner.nextInt();
@@ -13,27 +15,27 @@ public class MatrixTranspose {
         int[][] originalMatrix = new int[m][n];
         int[][] transposedMatrix = new int[n][m];
 
-        System.out.println("Введіть елементи матриці:");
+        System.out.println("Оригінальна матриця:");
 
-        // Заповнення та виведення оригінальної матриці
+        // Заповнення оригінальної матриці випадковими числами та транспонування
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                originalMatrix[i][j] = scanner.nextInt();
-            }
-        }
-
-        // Транспонування матриці
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+                originalMatrix[i][j] = random.nextInt(100); // Випадкове число від 0 до 99
+                System.out.print(originalMatrix[i][j] + " ");
                 transposedMatrix[j][i] = originalMatrix[i][j];
             }
+            System.out.println();
         }
 
-        // Виведення транспонованої матриці
         System.out.println("Транспонована матриця:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(transposedMatrix[i][j] + " ");
+        printMatrix(transposedMatrix);
+    }
+
+    // Метод для виведення матриці
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
